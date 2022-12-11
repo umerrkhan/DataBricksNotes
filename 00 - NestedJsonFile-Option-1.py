fe8a6a4f70356567bd7e928b5f5bc3c43fa5787e
Omer Farooq <44162521+umerrkhan@users.noreply.github.com>
2022-12-11 20:38:19 +0000

    Create 00 - NestedJsonFile-Option-1.py

---
+++ 00 - NestedJsonFile-Option-1.py
@@ -0,0 +1,43 @@
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType,LongType,ArrayType,DoubleType,DateType,IntegerType,ArrayType
+df = spark.read.option("multiline",True).json("/FileStore/tables/orderDetails.json")
+
+df = df.withColumn("orders",explode(col("datasets"))).select("filename", \
+                                                             "orders.orderId", \
+                                                             "orders.customerId", \
+                                                             "orders.orderDate", \
+                                                             "orders.shipmentDetails", \
+                                                             "orders.orderDetails")
+
+df = df.withColumn("shipmentDetails",col("shipmentDetails")).select( "filename", \
+                                                                     "orderId", \
+                                                                     "customerId", \
+                                                                     "orderDate", \
+                                                                     "shipmentDetails.*", \
+                                                                     "orderDetails")
+
+df = df.withColumn("orderDetails",explode(col("orderDetails"))).select("filename", \
+                                                                       "orderId", \
+                                                                       "customerId", \
+                                                                       "orderDate", \
+                                                                       "street", \
+                                                                       "city", \
+                                                                       "state", \
+                                                                       "postalCode", \
+                                                                       "country", \
+                                                                       "orderDetails.*")
+
+df = df.withColumn("orderDetails",col("totalPrice")).select("filename", \
+                                                            "orderId", \
+                                                            "customerId", \
+                                                            "orderDate", \
+                                                            "street", \
+                                                            "city", \
+                                                            "state", \
+                                                            "postalCode", \
+                                                            "country", \
+                                                            "productId", \
+                                                            "quantity", \
+                                                            "sequence", \
+                                                            "totalPrice.*" )
+
+display(df)
